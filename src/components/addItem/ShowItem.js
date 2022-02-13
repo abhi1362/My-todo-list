38fafbcdc4a749eb178b1735dc08a4cad44c3b1b
Abhishek Nayan <abhisheknayan21@gmail.com>
2022-02-13 22:33:15 +0530

    added update feature in todo app

--- src/components/addItem/ShowItem.js
+++ src/components/addItem/ShowItem.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './ShowItem.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const ShowItem = ({itemArray, deleteItem}) =>{
+const ShowItem = ({itemArray, deleteItem, editItem, toggleButton, completedTasks}) =>{
     return (
         <>
             <div className="show-items">
             {
-                itemArray.map((element ,id)=>{
+                itemArray.map((element)=>{
                     return(
-                        <div className="each-item" key={id}>
-                            <h3>{element}</h3>
-                            <FontAwesomeIcon icon={faTrash} title='Delete Item' className='delete-icon' onClick={()=>deleteItem(id)}/>
+                        <div className={`each-item ${toggleButton ? '' : 'disable-each-item'} ${element.value ? 'strike' : ''}`} key={element.id}>
+                            <div className='heading-div'>
+                                <div className={`heading-div-child1 ${element.value ? 'completed-task' : ''}`}>
+                                    <input type="checkbox" id={element.id} value={element.value} checked={element.value} onChange={()=>completedTasks(element.id)}/>
+                                    <label htmlFor={element.id}></label>
+                                </div>
+                                <div className="heading-div-child2">
+                                    <span className='date-time'> <span className='date-time-title'>Date :</span> { element.date } </span>
+                                    <span className='date-time'><span className='date-time-title'>Time :</span> { element.time }</span>
+                                    <h3>{element.name}</h3>
+                                </div>
+                            </div>
+                            <div className='icons-button'>
+                                <FontAwesomeIcon icon={faEdit} title='Edit Item' className={`edit-icon ${element.value ? 'completed-task' : ''}`} onClick={()=>editItem(element.id)}/>
+                                <FontAwesomeIcon icon={faTrash} title='Delete Item' className='delete-icon' onClick={()=>deleteItem(element.id)}/>
+                            </div>
                         </div>
                     )
                 })
--- src/components/toDoList/ToDoList.js
+++ src/components/toDoList/ToDoList.js
@@ -3,7 +3,7 @@ import todo from '../../assets/to-do-icon.png';
 import './ToDoList.css';
 import ShowItem from '../addItem/ShowItem.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faEdit } from '@fortawesome/free-solid-svg-icons'
 
 const getLocalItems = () =>{
     const lists = localStorage.getItem('lists');
@@ -14,26 +14,65 @@ const getLocalItems = () =>{
 }
 
 const ToDoList = () =>{
-    const [inputItem , setInputItem] = useState('');
-    const [itemArray , setItemArray] = useState(getLocalItems());
+    const [inputItem, setInputItem] = useState('');
+    const [itemArray, setItemArray] = useState(getLocalItems());
+    const [toggleButton, setToggleButton] = useState(true);
+    const [selectedId, setSelectedId] = useState(null);
     const addItems = () =>{
-        // console.log('added');
-        // const newItem = {
-        //     id: new Date().toString.getTime(),
-        //     name: ''
-        // }
-        // console.log(newItem.id)
-        if(inputItem){
-            setItemArray([...itemArray,inputItem]);
+        const newItem = {
+            id: Math.floor(Math.random() * 1000000000),
+            date: new Date().toDateString(),
+            time: new Date().toLocaleTimeString(),
+            name: inputItem,
+            value: false
+        }
+        if(inputItem && toggleButton){
+            setItemArray([...itemArray,newItem]);
             setInputItem('');
+        }
+        else if(inputItem && !toggleButton){
+            // setInputItem(selectedId.name);
+            setItemArray(
+                itemArray.map((elem)=>{
+                    if(elem.id === selectedId){
+                        return {...elem , name: inputItem}
+                    }
+                    return elem;
+                })
+            );
+            setSelectedId(null);
+            setToggleButton(true);
+            setInputItem('');
+        }
+        else{
+            if(toggleButton)
+                alert('Please enter an Item to Add');
+            else alert('Please enter an Item to Update');
         }
     }
     const deleteItem = (id) =>{
-        console.log('delete');
-        const updatedItem = itemArray.filter((ele,index)=>{
-            return index !== id;
+        const updatedItem = itemArray.filter((el)=>{
+            return el.id !== id;
         })
         setItemArray(updatedItem);
+    }
+    const editItem = (id) =>{
+        const selectedItem = itemArray.find(ele =>{
+            return ele.id === id;
+        })
+        setSelectedId(selectedItem.id);
+        setToggleButton(false);
+        setInputItem(selectedItem.name);
+    }
+    const completedTasks = (id) =>{
+        setItemArray(
+            itemArray.map((elem)=>{
+                if(elem.id === id){
+                    return {...elem , value: !elem.value}
+                }
+                return elem;
+            })
+        );
     }
     useEffect(() =>{
         localStorage.setItem('lists', JSON.stringify(itemArray));
@@ -48,11 +87,16 @@ const ToDoList = () =>{
                     </figure>
                     <div className='add-items'>
                         <input type='text' placeholder="Add items..." value={inputItem} onChange={(e) => setInputItem(e.target.value)} onKeyPress={e => e.key === 'Enter' ? addItems() : null }/>
-                        <FontAwesomeIcon icon={faPlus} title='Add Item' className='plus-icon' onClick={addItems}/>
+                        {
+                            toggleButton ? <FontAwesomeIcon icon={faPlus} title='Add Item' className='plus-icon' onClick={addItems}/> : <FontAwesomeIcon icon={faEdit} title='Update Item' className='plus-icon update-icon' onClick={addItems}/>
+                        }
                     </div>
                 </div>
             </div>
-            <ShowItem itemArray={itemArray} deleteItem={deleteItem} /> 
+            <ShowItem itemArray={itemArray} deleteItem={deleteItem} editItem={editItem} toggleButton={toggleButton} completedTasks={completedTasks}/> 
+            <div>
+            <button className={`clear-btn ${itemArray.length === 0 ? 'disabled-btn' : ''}`} disabled={itemArray.length === 0} onClick={()=> setItemArray([])}>Clear All</button>
+            </div>
         </>
     )
 }
